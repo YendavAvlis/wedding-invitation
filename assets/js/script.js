c4f2ea49670a3d0241ac5836d215545a093e51f2
YendavAvlis <vadneysilva@hotmail.com>
2024-06-19 08:52:32 +0200

    Time added

--- assets/js/script.js
+++ assets/js/script.js
@@ -262,3 +262,28 @@ const countdown = () => {
 };
 
 setInterval(countdown, 1000)
+
+//Counter
+
+const countdown1 = () => {
+  const countDate = new Date("July 3, 2024 10:00:00").getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const textDay = Math.floor(gap / day)
+  const textHour = Math.floor((gap % day) / hour)
+  const textMinute = Math.floor((gap % hour) / minute)
+  const textSecond = Math.floor((gap % minute) / second)
+
+  document.querySelector('.day1').innerText = textDay;
+  document.querySelector('.hour1').innerText = textHour;
+  document.querySelector('.minute1').innerText = textMinute;
+  document.querySelector('.second1').innerText = textSecond;
+};
+
+setInterval(countdown1, 1000)
